4b7da36b48ae748dfb0b77a20337424bc83c918c
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-08-27 01:31:58 +0900

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import DefaultLayout from '~/layouts/Default.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+  Vue.use(BootstrapVue)
 
   head.link.push({
     rel: 'stylesheet',
